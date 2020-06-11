8205e9ffbe5c2cb862a80987c96b99e9c42a6627
Азалия Смарагдова <charming.flurry@yandex.ru>
2020-06-11 12:29:54 +0000

    Bug fix

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 minetest.register_abm({
 	label="icemelt",
-	nodenames="default:ice",
+	nodenames={"default:ice"},
 	interval=30.0,
-	chance=4
-	action = function(pos, node, active_object_count, active_object_count_wider)
+	chance=4,
+	action=function(pos, node, active_object_count, active_object_count_wider)
 		if (minetest.get_node_light(pos)>=12) then
 			set_node("default:water_source")
 		end

fee124b9ed280ccdd4e086150f8d2312bcb45334
Азалия Смарагдова <charming.flurry@yandex.ru>
2020-06-11 12:32:38 +0000

    Corrected minetest.set_node syntax

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ minetest.register_abm({
 	chance=4,
 	action=function(pos, node, active_object_count, active_object_count_wider)
 		if (minetest.get_node_light(pos)>=12) then
-			minetest.set_node("default:water_source")
+			minetest.set_node(pos,{name="default:water_source"})
 		end
 	end
 })
